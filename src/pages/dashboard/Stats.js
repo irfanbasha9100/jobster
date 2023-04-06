f61f4b8617fc9bd974a19b9bd84210aacfb58000
irfanbasha9100 <irfanbashaparigi9133@gmail.com>
2023-04-06 12:47:48 +0530

    Errors changed

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -3,7 +3,7 @@ import { StatsContainer, Loading, ChartsContainer } from '../../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { showStats } from '../../features/allJobs/allJobsSlice';
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
+const { isLoading,/*monthlyApplications*/ } = useSelector(
     (store) => store.allJobs
   );
   const dispatch = useDispatch();
